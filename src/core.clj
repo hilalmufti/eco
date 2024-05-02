b7a68e1c4debdfbbf0a046d9ca2e500ad4cd5d8e
Hilal Mufti <hmufti@cs.washington.edu>
2024-05-02 00:51:04 -0700

    +sigmoid

--- src/core.clj
+++ src/core.clj
@@ -3,6 +3,9 @@
 (defn dot [v1 v2]
   (reduce + (map * v1 v2)))
 
+(defn sigmoid [z]
+  (/ 1 (+ 1 (Math/exp (- z)))))
+
 
 ; v1: autodiff with native clojure datatypes 
 ; v2: differentiation of clojure functions 
